d4d4115d6982dae0e1d77957ffabe75a0771ed31
pete79cy <panayiotis.pakkoutis@gmail.com>
2026-05-20 21:59:46 +0300

    chore: remove stale tsc-emitted .js artefacts

--- src/pages/Calendar.js
+++
@@ -1,2 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function Calendar() { return _jsx("div", { className: "p-4", children: "Calendar" }); }
--- src/pages/OrderDetail.js
+++
@@ -1,2 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function OrderDetail() { return _jsx("div", { className: "p-4", children: "OrderDetail" }); }
